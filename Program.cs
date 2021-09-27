484719fdae780310fa70e0ad99c079602671eb12
Ilya Kosykh <ilyuha.prosto.22041961@gmail.com>
2021-09-28 01:30:53 +0300

    Serialization done

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using TracerNS;
 using System.Collections.Generic;
 using System.Threading;
+using System.IO;
 
 namespace SPP1
 {
@@ -58,12 +59,31 @@ namespace SPP1
             Tracer tracer = new Tracer();
             Foo foo = new Foo(tracer);
             foo.MyMethod();
-            foo.MyMethod();
+            //foo.MyMethod();
+            
+            var root = tracer.GetTraceResult();
 
-            var temp = tracer.GetTraceResult();
+            
+
+            SerializerInJson ser = new SerializerInJson();
+            ser.Serialize(Console.OpenStandardOutput(), root);
+
+            using (FileStream fs = new FileStream("test.json", FileMode.OpenOrCreate))
+            {
+                ser.Serialize(fs, root);
+            }
+
+            SerializerInXml serXml = new SerializerInXml();
+            Console.WriteLine();
+            serXml.Serialize(Console.OpenStandardOutput(), root);
+
+            using (FileStream fs = new FileStream("test.xml", FileMode.OpenOrCreate))
+            {
+                serXml.Serialize(fs, root);
+            }
+            //Stream stream = Console.OpenStandardOutput();
 
 
-            Console.WriteLine("Hello World!");
         }
     }
 }

95cc7e4fbd21e70fa27341265ba13034ed37872b
Ilya Kosykh <ilyuha.prosto.22041961@gmail.com>
2021-09-28 14:26:45 +0300

    fixed folders

---
+++ Tracer/Serializers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TracerNS
+{
+
+    using System.IO;
+    using System.Text.Json;
+    using System.Xml.Serialization;
+
+    public  interface ISerializer
+    {
+        public void Serialize(System.IO.Stream serializationStream, TraceResult root);
+    }
+
+    public class SerializerInJson : ISerializer
+    {
+        
+
+        public void Serialize(System.IO.Stream serializationStream, TraceResult root)
+        {
+            string jsonString = JsonSerializer.Serialize(root, typeof(TraceResult));
+            //jsonString = jsonString.Replace("[", "\n[");
+            //jsonString = jsonString.Replace("]", "\n]");
+            //jsonString = jsonString.Replace("{", "\n{");
+            //jsonString = jsonString.Replace("}", "\n}");
+            //jsonString = jsonString.Replace(",", "\n,");
+            var stream = new StreamWriter(serializationStream);
+            stream.AutoFlush = true;
+            stream.Write(jsonString);
+        }
+    }
+
+    public class SerializerInXml : ISerializer
+    {
+        public void Serialize(System.IO.Stream serializationStream, TraceResult root)
+        {
+            //XmlSerializer formatter = new XmlSerializer(typeof(TraceResult));
+            List<Entry> entries = new List<Entry>(root.threadsInfo.Count);
+            foreach (int key in root.threadsInfo.Keys)
+            {
+                entries.Add(new Entry(key, root.threadsInfo[key]));
+            }
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Entry>));
+            formatter.Serialize(serializationStream, entries);
+        }
+    }
+
+    public class Entry
+    {
+        public int Key { get; set; }
+        [XmlElement(ElementName = "Thread")]
+        //[XmlInclude(typeof(ThreadInfo))]
+        public ThreadInfo Value { get; set; }
+        public Entry()
+        {
+        }
+
+        public Entry(int key, ThreadInfo value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
---
+++ Tracer/Tracer.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TracerNS
+{
+    using System.Text.Json.Serialization;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+
+    public interface ITracer
+    {
+        void StartTrace();
+    
+        void StopTrace();
+
+        TraceResult GetTraceResult();
+    }
+
+    [Serializable]
+    public class MethodInfo
+    {
+        
+        private Stopwatch stopwatch;
+        [XmlAttribute]
+        public long timeInterval { get; set; }
+
+        [JsonInclude]
+        public string className {  get; set; }
+        [JsonInclude]
+        public string methodName {  get; set; }
+
+        [JsonIgnore]
+        [XmlIgnore]
+        public MethodInfo parentMethod { get; private set; }
+
+        public List<MethodInfo> methods { get; private set; }
+
+        public MethodInfo()
+        {            
+        }
+
+        public MethodInfo(string _className, string _methodName, MethodInfo _parentMethod)
+        {
+            className = _className;
+            methodName = _methodName;
+            parentMethod = _parentMethod;
+            stopwatch = new Stopwatch();
+        }
+
+        public void AddSubMethod(MethodInfo subMethod)
+        {
+            if(methods == null)
+            {
+                methods = new List<MethodInfo>();
+            }
+            methods.Add(subMethod);
+        }
+
+        internal void StartTimer()
+        {
+            stopwatch.Start();
+        }
+
+        internal void StopTimer()
+        {
+            stopwatch.Stop();
+            timeInterval = stopwatch.ElapsedMilliseconds;
+        }
+    }
+
+    [Serializable]
+    public class ThreadInfo
+    {
+        
+        private Stopwatch stopwatch;
+        [XmlElement(ElementName = "time")]
+        public long timeInterval { get; set; }
+
+        [XmlElement(ElementName = "methods")]
+        public List<MethodInfo> methods { get; private set; }
+        
+        public ThreadInfo()
+        {
+            methods = new List<MethodInfo>();
+            stopwatch = new Stopwatch();
+        }
+
+        internal void StartTimer()
+        {
+            stopwatch.Start();
+        }
+
+        internal void StopTimer()
+        {
+            stopwatch.Stop();
+            timeInterval = stopwatch.ElapsedMilliseconds;
+        }
+
+        internal void AddMethod(MethodInfo _method)
+        {
+            if (_method != null)
+            {
+                methods.Add(_method);
+            }
+        }
+    }
+
+    
+
+    [Serializable]
+    public class TraceResult
+    {        
+        public Dictionary<int, ThreadInfo> threadsInfo { get; private set; }
+
+        
+        internal TraceResult()
+        {
+            threadsInfo = new Dictionary<int, ThreadInfo>();
+        }
+
+        internal void AddThread(int id, ThreadInfo _thread)
+        {
+            if (!isThreadExist(id)) 
+            {
+                threadsInfo.Add(id, _thread);
+            }
+        }
+
+        internal bool isThreadExist(int id)
+        {
+           return threadsInfo.ContainsKey(id);
+        }
+
+        internal ThreadInfo getThreadInfo(int id)
+        {
+            if (isThreadExist(id))
+            {
+                return threadsInfo[id];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public class Tracer : ITracer
+    {
+        static private object GetTracerLocker = 0;
+        static private object AddThreadLocker = 0;
+        private TraceResult traceResult;
+        private int counter = -1;
+
+        private MethodInfo currentMethod;
+
+        StackTrace st;
+        private static Tracer tracer = null;
+
+        static public Tracer GetTracer()
+        {
+            if (tracer != null)
+            {
+                return tracer;
+            }
+            else
+            {
+                lock (GetTracerLocker)
+                {
+                    if (tracer != null)
+                    {
+                        return tracer;
+                    }
+                    else
+                    {
+                        return tracer = new Tracer();
+                    }
+                }
+            }
+        }
+
+        public Tracer()
+        {
+            traceResult = new TraceResult();
+        }
+
+        public void StartTrace()
+        {
+            counter++;            
+
+            st = new StackTrace(false);
+            string typeName = st.GetFrame(1).GetMethod().DeclaringType.Name;
+            string methodName = st.GetFrame(1).GetMethod().ToString();
+            MethodInfo mInfo = new MethodInfo(typeName, methodName, currentMethod);
+            mInfo.StartTimer();
+            if (currentMethod == null)
+            {
+                int id = Thread.CurrentThread.ManagedThreadId;
+                ThreadInfo tInfo = traceResult.getThreadInfo(id);
+                if (tInfo == null)
+                {
+                    tInfo = new ThreadInfo();
+                    tInfo.StartTimer();
+                    lock (AddThreadLocker) 
+                    {
+                        traceResult.AddThread(id, tInfo);
+                    }
+                }
+                tInfo.StartTimer();
+
+                tInfo.AddMethod(mInfo);
+                currentMethod = mInfo;
+            }
+            else
+            {
+                currentMethod.AddSubMethod(mInfo);                
+                currentMethod = mInfo;
+            }
+            
+        }
+
+        public void StopTrace()
+        {
+            counter--;            
+            currentMethod.StopTimer();
+            currentMethod = currentMethod.parentMethod;
+            if (counter < 0 || currentMethod == null) 
+            {
+                int id = Thread.CurrentThread.ManagedThreadId;
+                ThreadInfo tInfo = traceResult.getThreadInfo(id);
+                tInfo.StopTimer();                
+            }
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return traceResult;
+        }
+    }
+}
